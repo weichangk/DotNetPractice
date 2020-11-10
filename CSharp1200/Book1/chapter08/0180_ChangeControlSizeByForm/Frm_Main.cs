204b5647f6eba8bea5d1f67f112f60e0f3b56093
weichangk <54664504+weichangk@users.noreply.github.com>
2020-11-10 23:27:49 +0800

    add CSharp1200 chapter08

---
+++ CSharp1200/Book1/chapter08/0180_ChangeControlSizeByForm/Frm_Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeControlSizeByForm
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
