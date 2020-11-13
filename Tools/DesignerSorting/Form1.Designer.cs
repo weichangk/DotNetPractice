eaccbd39b6bf85f1d7cba70a7c76a7ad17bc99c0
weichangk <54664504+weichangk@users.noreply.github.com>
2020-11-13 18:10:12 +0800

    modified DesignerSorting

--- Tools/DesignerSorting/Form1.Designer.cs
+++ Tools/DesignerSorting/Form1.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnSelect = new System.Windows.Forms.Button();
+            this.TBSelectFilePath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 84);
+            this.button1.Location = new System.Drawing.Point(588, 28);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +46,78 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(489, 401);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(588, 55);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(489, 401);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnSelect
+            // 
+            this.BtnSelect.Location = new System.Drawing.Point(24, 26);
+            this.BtnSelect.Name = "BtnSelect";
+            this.BtnSelect.Size = new System.Drawing.Size(76, 23);
+            this.BtnSelect.TabIndex = 4;
+            this.BtnSelect.Text = "选择文件";
+            this.BtnSelect.UseVisualStyleBackColor = true;
+            this.BtnSelect.Click += new System.EventHandler(this.BtnSelect_Click);
+            // 
+            // TBSelectFilePath
+            // 
+            this.TBSelectFilePath.Location = new System.Drawing.Point(111, 28);
+            this.TBSelectFilePath.Name = "TBSelectFilePath";
+            this.TBSelectFilePath.Size = new System.Drawing.Size(403, 21);
+            this.TBSelectFilePath.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1107, 473);
+            this.Controls.Add(this.TBSelectFilePath);
+            this.Controls.Add(this.BtnSelect);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnSelect;
+        private System.Windows.Forms.TextBox TBSelectFilePath;
     }
 }
 
--- Tools/DesignerSorting/Form1.cs
+++ Tools/DesignerSorting/Form1.cs
@@ -14,84 +14,142 @@ namespace DesignerSorting
 {
     public partial class Form1 : Form
     {
-        private string path;
-        private long lines;
-        private List<string> fileList = new List<string>();
 
-        private List<string> ss = new List<string>();
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void BtnSelect_Click(object sender, EventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog
             {
                 Multiselect = false,
                 Title = "请选择文件",
-                Filter = "Visual C# 文件(*.Designer*)|*.Designer*"
+                //Filter = "Visual C# 文件(*.Designer*)|*.Designer*"
             };
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                path = dialog.FileName;
+                TBSelectFilePath.Text = dialog.FileName;
             }
-            else
-            {
-                MessageBox.Show("未选中txt文件");
-            }
-
-            using (var sr = new StreamReader(path))
+        }
+        private Dictionary<string, List<string>> DataLoadToDictionary(string path)
+        {
+            List<string> fileList = new List<string>();
+            List<string> linesList = new List<string>();
+            StringBuilder lineTemp = new StringBuilder();
+            string objectName = string.Empty;
+            bool begin = false;
+            int lines;
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
             {
                 lines = 1;
-                while (sr.ReadLine() != null)
+                while (!sr.EndOfStream)
                 {
                     lines++;
                     fileList.Add(sr.ReadLine());
                 }
             }
-            Debug.WriteLine(lines);
-
-            bool begin =false;
-            bool end = false;
-            using (var sr = new StreamReader(path))
+            //Debug.WriteLine(lines);
+            for (int i = 0; i < lines; i++)
             {
-                while (sr.ReadLine() != null)
+                //Debug.WriteLine(fileList[i]);
+                if (fileList[i].Trim() == @"private void InitializeComponent()")
                 {
-                    if (sr.ReadLine() == "private void InitializeComponent()")
-                    {
-                        begin = true;                    
-                    }
+                    begin = true;
+                    objectName = "newObject";
+                    i += 1;
+                    continue;
+                }
 
-                    if (begin && !end)
-                    {
-                        fileList.Add(sr.ReadLine());
-                        if (sr.ReadLine() == @"\\")
-                        {
-                            end = true;
-                        }
-                    }
-                    
+                if (begin && fileList[i].Trim() == "}")
+                {
+                    dictionary.Add(objectName, linesList);
+                    //linesList.Clear();
+                    return dictionary;
+                }
 
+                if (!begin)
+                {
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(fileList[i]))
+                {
+                    continue;
+                }
+
+                lineTemp.Append(fileList[i]);
+                if (lineTemp.ToString().Contains(";") || lineTemp.ToString().Contains(@"//"))
+                {
+                    linesList.Add(lineTemp.ToString());
+                    lineTemp.Clear();
+                }
+
+                if (!fileList[i].Contains(@"//") && fileList[i + 1].Contains(@"//") && fileList[i + 2].Contains(@"//") && fileList[i + 3].Contains(@"//"))
+                {
+                    dictionary.Add(objectName, linesList);
+                    linesList.Clear();
+                    objectName = fileList[i + 2].Replace(@"//", "").Trim();
                 }
             }
+            return dictionary;
+        }
+        private List<string> DataSortingToList(Dictionary<string, List<string>> data)
+        {
+            List<string> list = new List<string>();
+            Dictionary<string, List<string>> dic1Asc = data.OrderBy(o => o.Key).ToDictionary(o => o.Key, p => p.Value);
+            foreach (var kv in dic1Asc)
+            {          
+                List<string> listTemp = new List<string>();
+                List<string> listTemp1 = new List<string>();
+                List<string> listTemp2 = new List<string>();         
+                for (int i = 0; i < kv.Value.Count; i++)
+                {
+                    if (i < 3)
+                    {
+                        listTemp1.Add(kv.Value[i]);
+                    }
+                    else
+                    {
+                        listTemp2.Add(kv.Value[i]);
+                    }    
+                }
+                listTemp2.Sort();
+                listTemp.AddRange(listTemp1);
+                listTemp.AddRange(listTemp2);
+                list.AddRange(listTemp);
+            }
+            return list;
+        }
 
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TBSelectFilePath.Text) || !File.Exists(TBSelectFilePath.Text))
+            {
+                MessageBox.Show("未选择正确文件路径");
+                return;
+            }
 
-            //int begin;
-            //int end;
+            Task.Factory.StartNew(() =>
+            {
+                Dictionary<string, List<string>> keyValuePairs = DataLoadToDictionary(TBSelectFilePath.Text);
+                if (keyValuePairs.Count > 0)
+                {
+                    List<string> list = DataSortingToList(keyValuePairs);
+                    foreach (var item in list)
+                    {
+                        richTextBox2.AppendText($"{item}{Environment.NewLine}");
+                    }
+                }
+            });
 
-            //for (int i = 1; i <= fileList.Count; i++)
-            //{
-            //    if (fileList[i] == "private void InitializeComponent()")
-            //    {
-            //        begin = i+1;
-            //    }
-            //    if (fileList[i] == @"//")
-            //    {
-            //        end = i;
-            //    }
-            //}
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
 
         }
     }
