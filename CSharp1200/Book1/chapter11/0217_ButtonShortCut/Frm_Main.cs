c6282f09275acdf19e1430622124c245093c1135
weichangk <54664504+weichangk@users.noreply.github.com>
2020-12-02 23:32:10 +0800

    add CSharp1200 chapter11 216-241

---
+++ CSharp1200/Book1/chapter11/0217_ButtonShortCut/Frm_Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ButtonShortCut
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(//弹出消息对话框
+              "点击了确定！", "提示！");
+        }
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(//弹出消息对话框
+             "点击了退出！", "提示！");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(//弹出消息对话框
+             "button1_Click！", "提示！");
+        }
+    }
+}
